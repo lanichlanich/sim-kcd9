49bc07ed02294d10d51a2394d4886f5d34a3d2a1
Farhany <lanichlanich@gmail.com>
2021-09-03 13:11:09 +0700

    menu dinamis

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -87,17 +87,26 @@
             <ul class="navigation navigation-main" id="main-menu-navigation" data-menu="menu-navigation">
                 <li class=" nav-item"><a href="#"><i class="feather icon-home"></i><span class="menu-title" data-i18n="Dashboard">Data Sekolah</span></a>
                     <ul class="menu-content">
-                        <li class="active"><a href="<?php echo base_url() ?>dashboard"><i class="feather icon-home"></i><span class="menu-item" data-i18n="Fixed navbar">Profile Sekolah</span></a>
+                        <li class="<?php if ($this->uri->segment(1) == "dashboard") {
+                                        echo "active";
+                                    } ?>"><a href="<?php echo base_url() ?>dashboard"><i class="feather icon-home"></i><span class="menu-item" data-i18n="Fixed navbar">Profile Sekolah</span></a>
                         </li>
-                        <li><a href="<?php echo base_url() ?>datasiswa"><i class="feather icon-users"></i><span class="menu-item" data-i18n="Floating navbar">Data Siswa</span></a>
+                        <li class="<?php if ($this->uri->segment(1) == "datasiswa") {
+                                        echo "active";
+                                    } ?>"><a href="<?php echo base_url() ?>datasiswa"><i class="feather icon-users"></i><span class="menu-item" data-i18n="Floating navbar">Data Siswa</span></a>
                         </li>
                     </ul>
                 </li>
                 <li class=" nav-item"><a href="#"><i class="feather icon-upload"></i><span class="menu-title">Import Data</span></a>
                     <ul class="menu-content">
-                        <li><a href="<?php echo base_url() ?>importsiswa"><i class="feather icon-upload"></i><span class="menu-item">Import Siswa</span></a>
+                        <li class="<?php if ($this->uri->segment(1) == "importsiswa") {
+                                        echo "active";
+                                    } ?>">
+                            <a href="<?php echo base_url() ?>importsiswa"><i class="feather icon-upload"></i><span class="menu-item">Import Siswa</span></a>
                         </li>
-                        <li><a href="<?php echo base_url() ?>importptk"><i class="feather icon-upload"></i><span class="menu-item">Import PTK</span></a>
+                        <li class="<?php if ($this->uri->segment(1) == "importptk") {
+                                        echo "active";
+                                    } ?>"><a href="<?php echo base_url() ?>importptk"><i class="feather icon-upload"></i><span class="menu-item">Import PTK</span></a>
                         </li>
                     </ul>
                 </li>
