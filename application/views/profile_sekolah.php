44db46cde00dc1bfe5b43b17a2ad84ccd7f44549
Farhany <lanichlanich@gmail.com>
2021-09-05 22:42:57 +0700

    Tambah Profile Sekolah untuk akun sekolah

---
+++ application/views/profile_sekolah.php
@@ -0,0 +1,198 @@
+    <!-- BEGIN: Content-->
+    <div class="app-content content">
+        <div class="content-overlay"></div>
+        <div class="header-navbar-shadow"></div>
+        <div class="content-wrapper">
+            <div class="content-body">
+                <!-- Description -->
+                <section id="description" class="card">
+                    <div class="card-header bg-secondary bg-darken-4">
+                        <h3 class="text-white">Profile Sekolah</h4>
+                    </div>
+                    <div class="card-content">
+                        <div class="card-body">
+                            <div class="row" id="table-hover-row">
+                                <div class="col-12">
+                                    <div class="card">
+                                        <div class="card-content">
+                                            <div class="table-responsive">
+                                                <table class="table table-hover display nowrap mb-0" id="sekolah">
+                                                    <tbody>
+                                                        <tr>
+                                                            <td class="bg-secondary bg-darken-2 h6" colspan="2">IDENTITAS SEKOLAH</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td class="table-secondary h5">Nama Sekolah</td>
+                                                            <td class="h6">tampil nama sekolah</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td class="table-secondary h6">NPSN</td>
+                                                            <td class="h6">tampil nama NPSN</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td class="table-secondary h6">Jenjang Pendidikan</td>
+                                                            <td class="h6">tampil nama Jenjang Pendidikan</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td class="table-secondary h6">Status Sekolah</td>
+                                                            <td class="h6">tampil nama Status Sekolah</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td class="table-secondary h6">Alamat Sekolah</td>
+                                                            <td class="h6">tampil nama Alamat Sekolah</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td class="table-secondary h6">RT/RW</td>
+                                                            <td class="h6">tampil nama RT/RW</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td class="table-secondary h6">Kode Pos</td>
+                                                            <td class="h6">tampil nama Kode Pos</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td class="table-secondary h6">Kelurahan</td>
+                                                            <td class="h6">tampil nama Kelurahan</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td class="table-secondary h6">Kecamatan</td>
+                                                            <td class="h6">tampil nama Kecamatan</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td class="table-secondary h6">Kabupaten</td>
+                                                            <td class="h6">tampil nama Kabupaten</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td class="table-secondary h6">Titik Koordinat</td>
+                                                            <td class="h6">tampil nama Titik Koordinat</td>
+                                                        </tr>
+
+                                                        <tr>
+                                                            <td class="bg-secondary bg-darken-2 h6" colspan="2">DATA PELENGKAP</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td class="table-secondary h6">SK Pendirian</td>
+                                                            <td class="h6">tampil SK Pendirian</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td class="table-secondary h6">Tanggal SK Pendirian</td>
+                                                            <td class="h6">tampil Tanggal SK Pendirian</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td class="table-secondary h6">Status Kepemilikan</td>
+                                                            <td class="h6">tampil Status Kepemilikan</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td class="table-secondary h6">SK Izin Operasional</td>
+                                                            <td class="h6">tampil SK Izin Operasional</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td class="table-secondary h6">Kebutuhan Khusus Dilayani</td>
+                                                            <td class="h6">tampil Kebutuhan Khusus Dilayani</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td class="table-secondary h6">Nomor Rekening Sekolah</td>
+                                                            <td class="h6">tampil Nomor Rekening Sekolah</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td class="table-secondary h6">Nama Bank</td>
+                                                            <td class="h6">tampil Nama Bank</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td class="table-secondary h6">Cabang KCP/Unit</td>
+                                                            <td class="h6">tampil Cabang KCP/Unit</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td class="table-secondary h6">Rekening Atas Nama</td>
+                                                            <td class="h6">tampil Rekening Atas Nama</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td class="table-secondary h6">MBS</td>
+                                                            <td class="h6">tampil MBS</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td class="table-secondary h6">Memungut Iuran</td>
+                                                            <td class="h6">tampil Memungut Iuran</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td class="table-secondary h6">Nominal/Siswa</td>
+                                                            <td class="h6">tampil Nominal/Siswa</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td class="table-secondary h6">Nama Wajib Pajak</td>
+                                                            <td class="h6">tampil Nama Wajib Pajak</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td class="table-secondary h6">NPWP</td>
+                                                            <td class="h6">tampil NPWP</td>
+                                                        </tr>
+
+                                                        <tr>
+                                                            <td class="bg-secondary bg-darken-2 h6" colspan="2">KONTAK SEKOLAH</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td class="table-secondary h6">Nomor Telepon</td>
+                                                            <td class="h6">tampil Nomor Telepon</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td class="table-secondary h6">Nomor Fax</td>
+                                                            <td class="h6">tampil Nomor Fax</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td class="table-secondary h6">Email</td>
+                                                            <td class="h6">tampil Email</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td class="table-secondary h6">Website</td>
+                                                            <td class="h6">tampil Website</td>
+                                                        </tr>
+
+                                                        <tr>
+                                                            <td class="bg-secondary bg-darken-2 h6" colspan="2">DATA PERIODIK</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td class="table-secondary h6">Waktu Penyelenggaraan</td>
+                                                            <td class="h6">tampil Waktu Penyelenggaraan</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td class="table-secondary h6">Bersedia Menerima BOS?</td>
+                                                            <td class="h6">tampil Bersedia Menerima BOS?</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td class="table-secondary h6">Sertifikat ISO</td>
+                                                            <td class="h6">tampil Sertifikat ISO</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td class="table-secondary h6">Sumber Listrik</td>
+                                                            <td class="h6">tampil Sumber Listrik</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td class="table-secondary h6">Daya Listrik</td>
+                                                            <td class="h6">tampil Daya Listrik</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td class="table-secondary h6">Akses Intenet</td>
+                                                            <td class="h6">tampil Akses Intenet</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td class="table-secondary h6">Akses Internet Alternatif</td>
+                                                            <td class="h6">tampil Akses Internet Alternatif</td>
+                                                        </tr>
+
+                                                    </tbody>
+                                                </table>
+                                                <button type="button" class="btn btn-success float-right mt-2" data-toggle="tooltip" data-placement="top" title="Edit"><i class="feather icon-edit"> </i>Edit</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <!--/ Description -->
+
+
+            </div>
+        </div>
+    </div>
+    <!-- END: Content-->
--- application/views/template/header.php
+++ application/views/template/header.php
@@ -91,7 +91,11 @@
                     <ul class="menu-content">
                         <li class="<?php if ($this->uri->segment(1) == "dashboard") {
                                         echo "active";
-                                    } ?>"><a href="<?php echo base_url() ?>dashboard"><i class="feather icon-home"></i><span class="menu-item" data-i18n="Fixed navbar">Profile Sekolah</span></a>
+                                    } ?>"><a href="<?php echo base_url() ?>dashboard"><i class="feather icon-home"></i><span class="menu-item" data-i18n="Fixed navbar">Dashboard</span></a>
+                        </li>
+                        <li class="<?php if ($this->uri->segment(1) == "profilesekolah") {
+                                        echo "active";
+                                    } ?>"><a href="<?php echo base_url() ?>profilesekolah"><i class="feather icon-home"></i><span class="menu-item" data-i18n="Fixed navbar">Profile Sekolah</span></a>
                         </li>
                         <li class="<?php if ($this->uri->segment(1) == "datasiswa") {
                                         echo "active";
