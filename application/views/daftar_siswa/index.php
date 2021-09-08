2a6b968ffaa8194f58bcd3db65b69a2bc5e4bc52
Farhany <lanichlanich@gmail.com>
2021-09-08 22:05:54 +0700

    Tambah Daftar Siswa hasil import

---
+++ application/views/daftar_siswa/index.php
@@ -0,0 +1,63 @@
+<!-- BEGIN: Content-->
+<div class="app-content content">
+    <div class="content-overlay"></div>
+    <div class="header-navbar-shadow"></div>
+    <div class="content-wrapper">
+
+        <div class="content-body">
+
+            <section id="description" class="card">
+
+                <div class="card-content">
+                    <div class="card-header bg-primary">
+                        <h4 class="mb-1" style="color: white;"><strong>Daftar Siswa</strong></h4>
+                    </div>
+                    <div class="card-body">
+                        <a href="daftarsiswa/#"><button class="btn btn-success">Tambah Siswa</button></a>
+                        <div class="row">
+
+
+                            <div class="table-responsive">
+                                <table class="table table-hover display nowrap mb-0" id="sekolah">
+                                    <thead>
+                                        <tr>
+                                            <th>Nama Siswa</th>
+                                            <th>NPSN</th>
+                                            <th>Jenis</th>
+                                            <th>Status</th>
+                                            <th>Kabupaten</th>
+                                            <th>Kabupaten</th>
+                                            <th>Alamat</th>
+                                            <th>Aksi</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php foreach ($daftar_siswa as $row) : ?>
+                                            <tr>
+                                                <td><?= $row->nama ?></td>
+                                                <td><?= $row->nipd ?></td>
+                                                <td><?= $row->nisn ?></td>
+                                                <td><?= $row->jk ?></td>
+                                                <td><?= $row->tempat_lahir ?></td>
+                                                <td><?= $row->tanggal_lahir ?></td>
+                                                <td><?= $row->rombel ?></td>
+                                                <td>
+                                                    <a href="daftarsiswa/#"><button class="btn btn-warning">Edit</button></a>
+                                                    <a href="daftarsiswa/#"><button class="btn btn-danger">Hapus</button></a>
+                                                </td>
+                                            </tr>
+                                        <?php endforeach; ?>
+
+                                    </tbody>
+                                </table>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+        </div>
+    </div>
+</div>
+<!-- END: Content-->
