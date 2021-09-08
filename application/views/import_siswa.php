75ac25283eed18f89bf3151b3d99a0dce05c8306
wawansompoxs <wawansompoxs@yahoo.co.id>
2021-09-08 20:49:00 +0700

    tambah import siswa excel

--- application/views/import_siswa.php
+++ application/views/import_siswa.php
@@ -1,33 +1,42 @@
     <!-- BEGIN: Content-->
     <div class="app-content content">
-        <div class="content-overlay"></div>
-        <div class="header-navbar-shadow"></div>
-        <div class="content-wrapper">
-            <div class="content-body">
-                <!-- Description -->
-                <section id="description" class="card">
-                    <div class="card-header">
-                        <h4 class="card-title">Import Data Siswa</h4>
-                    </div>
-                    <div class="card-content">
-                        <div class="card-body">
-                            <h6><i>Download Template Excel: <a href="#">Template Siswa.xls</a></i></h6>
-                            <div class="row" id="table-hover-row">
-                                <div class="col-12">
-                                    <div class="card">
-                                        <div class="card-content">
-                                            <input type="file">
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </section>
-                <!--/ Description -->
+    	<div class="content-overlay"></div>
+    	<div class="header-navbar-shadow"></div>
+    	<div class="content-wrapper">
+    		<div class="content-body">
+    			<!-- Description -->
+    			<section id="description" class="card">
+    				<div class="card-header">
+    					<h4 class="card-title">Import Data Siswa</h4>
+    				</div>
+    				<div class="card-content">
+    					<div class="card-body">
+							<?php
+							if($this->session->flashdata('message'))
+							{
+							echo $this->session->flashdata('message');
+							}
+							?>
+    						<h6><i>Download Template Excel: <a href="#">Template Siswa.xls</a></i></h6>
+    						<div class="row" id="table-hover-row">
+    							<form method="post" action="<?php echo base_url('importsiswa/spreadsheet_import');?>" class="" enctype="multipart/form-data">
+    								<div class="col-12">
+    									<div class="card">
+    										<div class="card-content">
+											<input type="file" name="upload_file" placeholder="Enter Name" id="upload_file" required>
+											<input type="submit" name="submit" class="btn btn-primary">
+    										</div>
+    									</div>
+    								</div>
+    							</form>
+    						</div>
+    					</div>
+    				</div>
+    			</section>
+    			<!--/ Description -->
 
 
-            </div>
-        </div>
+    		</div>
+    	</div>
     </div>
     <!-- END: Content-->
