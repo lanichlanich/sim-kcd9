b5d42721f955187081ea435bac1faa927ebbdc6e
Farhany <lanichlanich@gmail.com>
2021-09-29 21:29:28 +0700

    restore-lokal

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://sim-kcd9-01.herokuapp.com/';
+$config['base_url'] = 'http://localhost/sim-kcd9/';
 
 /*
 |--------------------------------------------------------------------------
